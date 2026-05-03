9650add508da3693f4a6ba6195bae8a004fdba7a
vighnesh204 <borhadevighnesh@gmail.com>
2026-05-03 12:16:17 +0530

    create home page and navbar update for fix scrolling issue

--- anime/src/app/page.js
+++ anime/src/app/page.js
@@ -1,9 +1,39 @@
-import React from 'react'
+'use client';
 
-const page = () => {
+import Image from "next/image";
+
+export default function Page() {
   return (
-    <div>page</div>
-  )
-}
+    <div className="relative w-full h-screen overflow-hidden">
 
-export default page
+      {/* Background Image */}
+      <Image
+        src="/tanjiro-kamado.jpg"
+        alt="Tanjiro"
+        fill
+        priority
+        className="object-cover"
+      />
+
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black/50"></div>
+
+      {/* Center Content */}
+      <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center text-white px-4">
+        
+        <h1 className="text-4xl md:text-7xl font-extrabold leading-tight">
+          Welcome to the Anime World
+        </h1>
+
+        <p className="mt-4 text-lg md:text-2xl text-gray-300 max-w-2xl">
+          Dive into epic battles, unforgettable characters, and legendary stories.
+        </p>
+
+        <button className="mt-6 px-6 py-3 bg-red-500 hover:bg-red-600 transition rounded-full text-lg font-semibold shadow-lg">
+          Explore Now ⚔️
+        </button>
+      </div>
+
+    </div>
+  );
+}
--- anime/src/components/Navbar.jsx
+++ anime/src/components/Navbar.jsx
@@ -7,17 +7,19 @@ import React from "react";
 const Navbar = () => {
     const pathname = usePathname();
   return (
-    <div className="w-full h-14 bg-white flex items-center justify-between px-4">
-      <div className="text-black font-bold text-2xl ">🚀 Anime</div>
+  <div className="absolute top-0 left-0 w-full h-14 z-50 flex items-center justify-between px-6">
+         <div className="text-white font-bold text-xl tracking-wide">
+        🚀 Anime
+      </div>
       <div>
         <ul className="flex items-center justify-center gap-4">
-           <Link href="/" className={pathname === "/" ? "text-blue-500" : "text-black"}>
+           <Link href="/" className={pathname === "/" ? "text-blue-500" : "text-white"}>
             <li>Home</li>
           </Link>
-          <Link href="/characters" className={pathname === "/characters" ? "text-blue-500" : "text-black"}>
+          <Link href="/characters" className={pathname === "/characters" ? "text-blue-500" : "text-white"}>
             <li>Characters</li>
           </Link>
-          <Link href="/episodes" className={pathname === "/episodes" ? "text-blue-500" : "text-black"}>
+          <Link href="/episodes" className={pathname === "/episodes" ? "text-blue-500" : "text-white"}>
             <li>Episodes</li>
           </Link>
         </ul>
