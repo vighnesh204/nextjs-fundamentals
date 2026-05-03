4c74ebc54bf356306a79e5c32721c80cb7da0bae
vighnesh204 <borhadevighnesh@gmail.com>
2026-05-03 11:35:48 +0530

    feat(navbar): highlight active route using usePathname hook

--- anime/src/components/Navbar.jsx
+++ anime/src/components/Navbar.jsx
@@ -1,15 +1,25 @@
+'use client';
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Navbar = () => {
+    const pathname = usePathname();
   return (
-    <div className="w-full h-[60px] bg-white flex items-center justify-between px-4">
+    <div className="w-full h-14 bg-white flex items-center justify-between px-4">
       <div className="text-black font-bold text-2xl ">🚀 Anime</div>
       <div>
         <ul className="flex items-center justify-center gap-4">
-           <Link href="/"><li>Home</li></Link>
-            <Link href="/characters"><li>Characters</li></Link>
-            <Link href="/episodes"><li>Episodes</li></Link>
+           <Link href="/" className={pathname === "/" ? "text-blue-500" : "text-black"}>
+            <li>Home</li>
+          </Link>
+          <Link href="/characters" className={pathname === "/characters" ? "text-blue-500" : "text-black"}>
+            <li>Characters</li>
+          </Link>
+          <Link href="/episodes" className={pathname === "/episodes" ? "text-blue-500" : "text-black"}>
+            <li>Episodes</li>
+          </Link>
         </ul>
       </div>
     </div>
